d2a25f1296b0d2929065039d7afe882e936cf2bf
Daria Ablovatskaia <daryana_chizhova@list.ru>
2021-05-27 17:06:47 +0700

    implement getLatestPosts() and getLatestPostComments() APIs

--- server.js
+++ server.js
@@ -4,48 +4,71 @@ const cors = require('@koa/cors');
 const uuid = require('uuid');
 const faker = require('faker');
 
+const POST_COUNT = 3;
+
+const generatePostComment = (postId) => ({
+  id: uuid.v4(),
+  post_id: postId,
+  author_id: faker.datatype.number(),
+  author: faker.internet.userName(),
+  avatar: faker.internet.avatar(),
+  content: faker.random.words(10),
+  created: faker.date.past(),
+});
+
+const generatePost = (id) => ({
+  id,
+  author_id: faker.datatype.number(),
+  title: faker.random.words(4),
+  author: faker.internet.userName(),
+  avatar: faker.internet.avatar(),
+  image: faker.image.cats(),
+  created: faker.date.past(),
+  comments: [
+    generatePostComment(id),
+    generatePostComment(id),
+    generatePostComment(id)
+  ],
+});
+
+const generatePosts = () => {
+  const res = [];
+  for (let id = 1; id <= POST_COUNT; id++) {
+    res.push(generatePost(id));
+  }
+  return res;
+};
+
+const posts = generatePosts();
+
 const app = new Koa();
 
 app.use(cors());
 
-let previousRequestTs = Date.now();
-
-const listOfRecentPosts  = (now) => {
-  const maxAge = now - previousRequestTs;
-  const messageAge = Math.floor(Math.random() * maxAge);
-  return {
-    id: uuid.v4(),
-    author_id: faker.internet.email(),
-    title: faker.random.words(4),
-    author: faker.internet.username(),
-    avatar: faker.internet.avatar(),
-    image: faker.internet.url() ,
-    created: now - messageAge
-};
-
-  const listOfRecentCommentsToThePost = (now) => {
-  };
-
-
-
-  const listOfRecentPosts = () => {
-  const timestamp = Date.now();
-  const messages = [
-    listOfRecentPosts(timestamp),
-    listOfRecentPosts(timestamp),
-  ];
-  previousRequestTs = timestamp;
-  return { status: 'ok', timestamp, messages };
-};
-
 app.use((ctx) => {
-  if (ctx.path !== '/messages/unread') {
-    ctx.response.status = 404;
+  if (ctx.path === '/posts/latest') {
+    ctx.body = {
+      status: 'ok',
+      data: posts.map(({ comments, ...post }) => post),
+    };
+    ctx.status = 200;
     return;
   }
 
-  ctx.body = listOfRecentPosts();
-  ctx.status = 200;
+  const match = ctx.path.match(/\/posts\/(\d+)\/comments\/latest/);
+  if (match) {
+    const postId = Number(match[1]);
+    const post = posts.find(({ id }) => id === postId);
+
+    ctx.body = {
+      status: 'ok',
+      data: post.comments,
+    };
+    ctx.status = 200;
+    return;
+  }
+
+  ctx.response.status = 404;
 });
 
 http.createServer(app.callback()).listen(process.env.PORT || 5555, () => console.log('Server is working'));
